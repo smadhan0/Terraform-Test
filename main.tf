edd67912934fc42c96c059cc09e567fe1a9dbbf4
smadhan0 <70846824+smadhan0@users.noreply.github.com>
2023-04-01 15:31:32 -0400

    Commit for AWS keys

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region  = "us-east-1"
   #shared_credentials_file = "$HOME/.aws/credentials" # default
-  access_key = "AKIA6JO2NIYX26K2IE46"
-  secret_key = "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"
+  access_key = "*********"
+  secret_key = "**********"
   #profile = "default" # you may change to desired profile
   #version = "~> 2.63"
 }
